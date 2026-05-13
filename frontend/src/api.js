dff504a0076d2c7775553a3ac8dac4cdd59a70a2
AnandSingh06 <Anandkumarsingh773@gmail.com>
2026-05-14 03:52:53 +0530

    Connected backend to live API

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8081",
+  baseURL: "https://mini-quora-backend.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
